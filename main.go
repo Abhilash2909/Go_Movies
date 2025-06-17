a31eb75297fe282c84acd7f63cc5ebb1b6762c11
Abhilash2909 <abhilashabhi2909@gmail.com>
2025-06-17 17:05:51 +0530

    changes

--- main.go
+++ main.go
@@ -1,111 +1,135 @@
 package main
 
+// Importing necessary standard and third-party packages
 import (
-	"encoding/json"
+	"encoding/json" // To handle JSON encoding and decoding
 	"fmt"
 	"log"
-	"math/rand"
-	"net/http"
-	"strconv"
+	"net/http" // For HTTP server and request/response handling
 
-	"github.com/gorilla/mux"
+	"github.com/google/uuid" // To generate unique IDs for each movie
+	"github.com/gorilla/mux" // To use the Mux router for handling routes with parameters
 )
 
+// Movie struct defines the structure of a movie object
 type Movie struct {
-	ID       string    `json:"id"`
-	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title"`
-	Director *Director `json:"director"`
+	ID       string    `json:"id"`       // Unique identifier for the movie
+	Isbn     string    `json:"isbn"`     // ISBN number of the movie
+	Title    string    `json:"title"`    // Title of the movie
+	Director *Director `json:"director"` // Director details embedded as a pointer
 }
 
+// Director struct defines the structure for director details
 type Director struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
+	Firstname string `json:"firstname"` // First name of the director
+	Lastname  string `json:"lastname"`  // Last name of the director
 }
 
+// A global slice to act as an in-memory database for storing movies
 var movies []Movie
 
+// Handler to return all movies as JSON
 func getMovies(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
+	w.Header().Set("Content-Type", "application/json") // Set response header to JSON
+	json.NewEncoder(w).Encode(movies)                  // Encode and send all movies
 }
 
+// Handler to return a single movie by ID
 func getMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, item := range movies {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item) // item
+	w.Header().Set("Content-Type", "application/json") // Set response header to JSON
+	params := mux.Vars(r)                              // Get route parameters from request (e.g., ID)
+	for _, item := range movies {                      // Iterate over all movies
+		if item.ID == params["id"] { // If movie ID matches
+			json.NewEncoder(w).Encode(item) // Encode and return the movie
 			return
 		}
 	}
-	http.Error(w, "Movie not found", http.StatusNotFound)
+	http.Error(w, "Movie not found", http.StatusNotFound) // Send 404 if not found
 }
 
+// Handler to delete a movie by ID
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range movies {
-		if item.ID == params["id"] {
-			// delete the movie with the ID that we sent
-			// append the movies slice without the deleted movie
+	w.Header().Set("Content-Type", "application/json") // Set response header to JSON
+	params := mux.Vars(r)                              // Get the ID from route parameters
+	for index, item := range movies {                  // Iterate over movies
+		if item.ID == params["id"] { // If movie ID matches
+			// Remove movie by slicing around the index
 			movies = append(movies[:index], movies[index+1:]...)
-			json.NewEncoder(w).Encode(movies)
+			json.NewEncoder(w).Encode(movies) // Return the updated movie list
 			return
 		}
 	}
-	// If the movie is not found, return an error
-	http.Error(w, "Movie not found", http.StatusNotFound)
+	http.Error(w, "Movie not found", http.StatusNotFound) // Send 404 if not found
 }
 
+// Handler to create a new movie entry
 func createMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var movie Movie
-	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = strconv.Itoa(rand.Intn(1000000))
-	movies = append(movies, movie)
-	json.NewEncoder(w).Encode(movie)
+	w.Header().Set("Content-Type", "application/json")             // Set response header to JSON
+	var movie Movie                                                // Define a variable to hold incoming JSON
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil { // Decode the request body
+		http.Error(w, "Invalid JSON", http.StatusBadRequest) // Return 400 if bad JSON
+		return
+	}
+	movie.ID = uuid.New().String()   // Generate a new unique ID for the movie
+	movies = append(movies, movie)   // Add the new movie to the slice
+	json.NewEncoder(w).Encode(movie) // Return the newly created movie
 }
 
+// Handler to update an existing movie by ID
 func updateMovie(w http.ResponseWriter, r *http.Request) {
-	// setting the json content type
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json") // Set response header to JSON
+	params := mux.Vars(r)                              // Get the movie ID from route
 
-	//looping over the movies using range
-	// delete the movie with the ID that we sent
-	// add a new movie that we send in the body of postman
-	for index, item := range movies {
-		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
+	for index, item := range movies { // Iterate through the movies
+		if item.ID == params["id"] { // Find the movie to update
+			movies = append(movies[:index], movies[index+1:]...) // Delete old movie
+
 			var movie Movie
-			if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
-				http.Error(w, "Invalid request body", http.StatusBadRequest)
+			if err := json.NewDecoder(r.Body).Decode(&movie); err != nil { // Decode new data
+				http.Error(w, "Invalid request body", http.StatusBadRequest) // Return error
 				return
 			}
-			movie.ID = params["id"]
-			movies = append(movies, movie)
-			json.NewEncoder(w).Encode(movie)
+
+			movie.ID = params["id"]          // Keep the same ID
+			movies = append(movies, movie)   // Add the updated movie
+			json.NewEncoder(w).Encode(movie) // Return the updated movie
 			return
 		}
 	}
-	// If the movie is not found, return an error
-	http.Error(w, "Movie not found", http.StatusNotFound)
-	json.NewEncoder(w).Encode(&Movie{}) // return an empty movie if not found
+	http.Error(w, "Movie not found", http.StatusNotFound) // Send 404 if not found
 }
 
+// main function sets up the router and starts the server
 func main() {
-	r := mux.NewRouter()
+	r := mux.NewRouter() // Create a new Gorilla Mux router
 
-	movies = append(movies, Movie{ID: "1", Isbn: "001", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
-	movies = append(movies, Movie{ID: "2", Isbn: "002", Title: "Movie Two", Director: &Director{Firstname: "Jane", Lastname: "Doe"}})
-	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
-	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	// Add some sample movies to the in-memory slice
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "001",
+		Title: "Movie One",
+		Director: &Director{
+			Firstname: "John",
+			Lastname:  "Doe",
+		},
+	})
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "002",
+		Title: "Movie Two",
+		Director: &Director{
+			Firstname: "Jane",
+			Lastname:  "Doe",
+		},
+	})
 
-	fmt.Println("Starting server on port 8000...")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	// Define the API routes and map them to their respective handler functions
+	r.HandleFunc("/movies", getMovies).Methods("GET")           // GET all movies
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")       // GET a single movie by ID
+	r.HandleFunc("/movies", createMovie).Methods("POST")        // POST a new movie
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")    // PUT to update a movie
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE") // DELETE a movie
 
+	fmt.Println("Starting server on port 8000...") // Print server start message
+	log.Fatal(http.ListenAndServe(":8000", r))     // Start the server and listen on port 8000
 }
